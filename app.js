f8db7431c7e4ec838e7dbeff9c9ebe208df9da36
Johan Bendz <34488771+JohanBendz@users.noreply.github.com>
2026-09-19 10:27:55 +0200

    Clean app bootstrap formatting

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 'use strict';
+
 const Homey = require('homey');
 
 class SWEFA extends Homey.App {
-
-	async onInit() {
-
-		this.log('SMHI weather forecasting app successfully started.');
-
-	};
-
+  async onInit() {
+    this.log('SMHI weather forecasting app successfully started.');
+  }
 }
+
 module.exports = SWEFA;
 
 // Some weather icons made by Freepik @ https://www.flaticon.com/
